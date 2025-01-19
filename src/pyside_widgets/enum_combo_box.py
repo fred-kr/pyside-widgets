0b8e41e9a0036704b2a9af8d62f8a0201a17e459
Frederik Krämer <frdrkkraemer@gmail.com>
2025-01-19 05:51:46 +0100

    Update EnumComboBox

--- src/pyside_widgets/enum_combo_box.py
+++ src/pyside_widgets/enum_combo_box.py
@@ -16,7 +16,7 @@ class EnumComboBox[T: enum.Enum](QtWidgets.QComboBox):
 
     Inspired by [`superqt.QEnumComboBox`](https://pyapp-kit.github.io/superqt/widgets/qenumcombobox/#qenumcombobox).
     """
-    sig_current_enum_changed = QtCore.Signal(enum.Enum)
+    sig_current_enum_changed = QtCore.Signal(object)
 
     def __init__(
         self,
@@ -28,16 +28,24 @@ class EnumComboBox[T: enum.Enum](QtWidgets.QComboBox):
 
         self._enum_class = enum_class
         self._enum_model = QtGui.QStandardItemModel(self)
+        self._allow_none = allow_none
 
         self.setPlaceholderText(PLACEHOLDER_TEXT)
 
-        self.set_enum_class(enum_class, allow_none=allow_none)
+        self.set_enum_class(enum_class)
         self.currentIndexChanged.connect(self._on_current_index_changed)
 
+    def allowNone(self) -> bool:
+        """Whether to allow the combo box to have no selection."""
+        return self._allow_none
+
+    def setAllowNone(self, allow_none: bool) -> None:
+        """Setter of property `allowNone`."""
+        self._allow_none = allow_none
+        
     def set_enum_class(
         self,
         enum_class: type[enum.Enum] | None,
-        allow_none: bool = False,
         text_data: Callable[[enum.Enum], str] | None = None,
         icon_data: Callable[[enum.Enum], QtGui.QIcon] | None = None,
     ) -> None:
@@ -46,7 +54,6 @@ class EnumComboBox[T: enum.Enum](QtWidgets.QComboBox):
 
         Args:
             enum_class: The enum class to be used in the combo box.
-            allow_none: If True, adds a placeholder item as the first item with text "<No Selection>" and value `None`.
             text_data: Optional callable to provide custom text for each enum member.
                 Defaults to None, using the enum member's name if not provided.
             icon_data: Optional callable to provide custom icon for each enum member.
@@ -65,7 +72,7 @@ class EnumComboBox[T: enum.Enum](QtWidgets.QComboBox):
                 item.setIcon(icon_data(enum_member))
             self._enum_model.appendRow(item)
 
-        if allow_none:
+        if self._allow_none:
             none_item = QtGui.QStandardItem(NO_SELECTION_TEXT)
             none_item.setData(None, role=ItemDataRole.UserRole)
             self._enum_model.insertRow(0, none_item)
@@ -82,31 +89,40 @@ class EnumComboBox[T: enum.Enum](QtWidgets.QComboBox):
         """
         if self._enum_class is None:
             return None
-        data = self.currentData(role=ItemDataRole.UserRole)
-        return None if data not in self._enum_class else data
+        if self._allow_none and self.currentIndex() == 0:
+            return None
+        return self.currentData(role=ItemDataRole.UserRole)
+        # return None if data not in self._enum_class else data
 
-    def set_current_enum(self, value: T) -> None:
+    def set_current_enum(self, value: T | None) -> None:
         """Sets the current combo box item to the provided enum member.
 
         Args:
             value (T): The enum member to be set as the current item.
         """
         if self._enum_class is None:
-            raise RuntimeError()
+            raise ValueError("Enum class not set")
+        if value is None and not self._allow_none:
+            raise ValueError("Cannot set None if allow_none is False")
         index = self.findData(value, role=ItemDataRole.UserRole)
         if index >= 0:
             self.setCurrentIndex(index)
 
     @QtCore.Slot(int)
     def _on_current_index_changed(self, index: int) -> None:
-        enum_member = self.current_enum()
-        if enum_member is not None:
-            self.sig_current_enum_changed.emit(enum_member)
+        # enum_member = self.current_enum()
+        # if enum_member is not None:
+        self.sig_current_enum_changed.emit(self.current_enum())
+
+    allowNone = QtCore.Property(bool, allowNone, setAllowNone)
 
 
 DOM_XML = """
 <ui language='c++'>
     <widget class='EnumComboBox' name='enumComboBox'>
+        <property name='allowNone'>
+            <bool>false</bool>
+        </property>
     </widget>
 </ui>
 """
@@ -118,7 +134,7 @@ class EnumComboBoxPlugin(QtDesigner.QDesignerCustomWidgetInterface):
         self._initialized = False
 
     def createWidget(self, parent: QtWidgets.QWidget) -> QtWidgets.QWidget:
-        return EnumComboBox(parent=parent, enum_class=None, allow_none=True)
+        return EnumComboBox(parent=parent)
 
     def domXml(self) -> str:
         return DOM_XML
