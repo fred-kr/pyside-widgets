ad311576c2ca55984ad03da38b6c9dac7e0dd66f
devduck <frederik.kraemer@dev-duck.dev>
2026-04-17 17:38:23 +0200

    Remove old registrars folder

--- src/pyside_widgets/registrars/register_color_picker_button.py
+++
@@ -1,7 +0,0 @@
-from PySide6 import QtDesigner
-
-from pyside_widgets.color_picker_button import ColorPickerButton  # noqa: F401 # type: ignore,
-from pyside_widgets.plugins.input_plugin import ColorPickerButtonPlugin
-
-if __name__ == "__main__":
-    QtDesigner.QPyDesignerCustomWidgetCollection.addCustomWidget(ColorPickerButtonPlugin())
--- src/pyside_widgets/registrars/register_toggle_switch.py
+++
@@ -1,7 +0,0 @@
-from PySide6 import QtDesigner
-
-from pyside_widgets.plugins.input_plugin import ToggleSwitchPlugin
-from pyside_widgets.toggle_switch import ToggleSwitch  # noqa: F401 # type: ignore,
-
-if __name__ == "__main__":
-    QtDesigner.QPyDesignerCustomWidgetCollection.addCustomWidget(ToggleSwitchPlugin())
