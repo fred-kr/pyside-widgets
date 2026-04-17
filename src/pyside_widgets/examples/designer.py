e3e1c5404a8f14f89975400f5a875044b9b83b73
devduck <frederik.kraemer@dev-duck.dev>
2026-04-17 18:19:14 +0200

    Add runner script, switch to subprocess.run for starting designer

--- src/pyside_widgets/examples/designer.py
+++ src/pyside_widgets/examples/designer.py
@@ -1,9 +1,16 @@
-import os
-from pathlib import Path
+def run():
+    import os
+    import subprocess
+    from pathlib import Path
 
-plugin_path = Path(__file__).parent.parent.joinpath("plugins")
-os.environ["PYSIDE_DESIGNER_PLUGINS"] = str(plugin_path)
+    plugin_path = Path(__file__).parent.parent.joinpath("plugins")
+    os.environ["PYSIDE_DESIGNER_PLUGINS"] = str(plugin_path)
 
-print(plugin_path)
+    print(plugin_path)
 
-os.system(f"cd {plugin_path} && pyside6-designer")
+    # os.system(f"cd {plugin_path} && pyside6-designer")
+    subprocess.run("pyside6-designer", cwd=plugin_path)
+
+
+if __name__ == "__main__":
+    run()
